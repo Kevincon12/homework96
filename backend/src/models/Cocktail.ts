2616ca633fc957e3c9ad97caa1fd666dc4e4b3d9
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-04-29 19:33:43 +0600

    Добавлена модель коктейлей

---
+++ backend/src/models/Cocktail.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from 'mongoose';
+
+const CocktailSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: null
+    },
+    recipe: {
+        type: String,
+        required: true
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    ingredients: [
+        {
+            name: String,
+            amount: String
+        }
+    ],
+    ratings: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            value: {
+                type: Number,
+                min: 1,
+                max: 5
+            }
+        }
+    ]
+});
+
+const CocktailModel = mongoose.model('Cocktail', CocktailSchema);
+
+export default CocktailModel;
